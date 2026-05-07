e89063a2eba4db8b97af1b6abd6a2e2b93cc448c
Daniel Salameh <danielsalameh04@gmail.com>
2026-05-07 10:37:45 -0700

    almost there on computer
    - fix not picking up input
    - fix camera rotation big

--- Modules/Interaction/Computer/computer.gd
+++ Modules/Interaction/Computer/computer.gd
@@ -3,7 +3,6 @@ extends Node3D
 class_name Computer
 
 @export var data: ComputerData
-@onready var desktop_viewer: DesktopViewer = $"Screen/SubViewport/Desktop Viewer"
 @onready var view_position: Marker3D = $ViewPosition
 
 var player_camera: Camera3D
@@ -12,6 +11,7 @@ var original_fov: float
 var is_viewing: bool = false
 var tween: Tween
 var computer_ui_instance: ComputerUI
+var target_fov : float = 90
 
 #func interact():
 	#if not data:
@@ -24,15 +24,6 @@ var computer_ui_instance: ComputerUI
 
 const COMPUTER_UI = preload("res://Modules/Interaction/Computer/computer_ui.tscn")
 
-func _ready() -> void:
-	# load the UI into the subviewport on ready, not on interact
-	# so it's always running in the background
-	computer_ui_instance = COMPUTER_UI.instantiate()
-	sub_viewport.add_child(computer_ui_instance)
-	computer_ui_instance.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
-	if data:
-		computer_ui_instance.load_computer(data)
-
 func interact() -> void:
 	if is_viewing:
 		return
@@ -42,7 +33,9 @@ func interact() -> void:
 
 func lerp_camera_to_screen() -> void:
 	is_viewing = true
-
+	#globals.player.disable_input()
+	globals.ui_manager.desktop_viewer.display_computer_on_model(self, sub_viewport, data)
+	
 	if tween:
 		tween.kill()
 	tween = create_tween()
@@ -54,10 +47,13 @@ func lerp_camera_to_screen() -> void:
 		view_position.global_transform,
 		0.6
 	)
-	globals.player_camera.fov = 100
-	tween.tween_callback(func(): 
-		desktop_viewer.display_computer(data)
+	tween.parallel().tween_property(
+		globals.player_camera,
+		"fov",
+		target_fov,
+		0.6
 	)
+	
 
 func close_computer() -> void:
 	if tween:
@@ -67,12 +63,26 @@ func close_computer() -> void:
 	tween.set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(
 		globals.player_camera,
-		"global_transform", 
+		"global_transform",
 		original_transform,
 		0.6
 	)
-	globals.player_camera.fov = original_fov
+	tween.parallel().tween_property(
+		globals.player_camera,
+		"fov",
+		original_fov,
+		0.6
+	)
 	tween.tween_callback(func():
 		is_viewing = false
-		globals.player.enable_input()
+		#globals.ui_manager.desktop_viewer.call_deffered("close")
+		print(sub_viewport.get_child_count())
+		if sub_viewport.get_child_count() != 0:
+			sub_viewport.get_child(0).queue_free()
 	)
+
+#func handle_input(_delta) -> void:
+	#if is_viewing:
+		#if Input.is_action_just_pressed("escape") or Input.is_action_just_pressed("player_interact"):
+			#print("COMPUTER HANDLED INPUT")
+			#close_computer()
